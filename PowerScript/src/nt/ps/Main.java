f7c10bf87d54292a1c1872e600359d78fd1da269
Marc Pascual Martinez <mpascualm17@gmail.com>
2017-12-08 13:51:53 +0100

    Main Changes

--- PowerScript/src/nt/ps/Main.java
+++ PowerScript/src/nt/ps/Main.java
@@ -5,8 +5,11 @@
  */
 package nt.ps;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import nt.ps.compiler.CompilerUnit;
 import nt.ps.compiler.exception.CompilerError;
 import nt.ps.compiler.exception.PSCompilerException;
 
@@ -18,12 +21,12 @@ public final class Main
 {
     public static void main(String[] args) throws CompilerError, FileNotFoundException, PSCompilerException, IOException
     {
-        CommandLineInterpreter cli = new CommandLineInterpreter();
-        cli.execute(args);
+        /*CommandLineInterpreter cli = new CommandLineInterpreter();
+        cli.execute(args);*/
         //cli.execute("test.pws");
         //return;
         
-        //CompilerError.setDebugMode(true);
+        CompilerError.setDebugMode(true);
         
         
         /*File file = new File("test.pws");
@@ -34,7 +37,7 @@ public final class Main
         PSScript script = CompilerUnit.compile(fis, state, cl, "test");
         script.execute();*/
         
-        /*CompilerUnit.compileAsJar(new File("test.jar"), new File(System.getProperty("user.dir")),
+        CompilerUnit.compileAsJar(new File("test.jar"), new File(System.getProperty("user.dir")),
                 new File("test.pws"));
         
         File file = new File("test.pws");
@@ -42,7 +45,6 @@ public final class Main
         PSClassLoader cl = new PSClassLoader(Main.class.getClassLoader());
         
         PSState state = PSState.createDefaultInstance();
-        state.insertDefaultImportFunction();
         state.insertDefaultIOUtils();
         //state.setGlobalValue("IO", new PSIO(state));
         //state.setGlobalValue("Object", new PSObjectReference());
@@ -56,6 +58,6 @@ public final class Main
         double t4 = System.nanoTime();
         
         System.out.println("Time to Compile: " + ((t2 - t1) / 1000000) + "ms");
-        System.out.println("Time to Execute: " + ((t4 - t3) / 1000000) + "ms");*/
+        System.out.println("Time to Execute: " + ((t4 - t3) / 1000000) + "ms");
     }
 }
